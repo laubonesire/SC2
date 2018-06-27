76cfefa6a350db25876b58d2c20393b454567d58
Laurent Bonesire <lauurent@gmail.com>
2018-06-27 14:54:38 +0200

    Army comp logic and attack logic reworked. Still not good as once initial attack is launched, will keep sending troops regardless of outcome

--- protoss/macrotoss.py
+++ protoss/macrotoss.py
@@ -4,7 +4,7 @@ import sc2
 import random
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, ZEALOT, STALKER
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, ZEALOT, STALKER, FORGE
 
 
 class MacroTossBot(sc2.BotAI):    
@@ -54,7 +54,7 @@ class MacroTossBot(sc2.BotAI):
                 if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
                     await self.build(CYBERNETICSCORE, near=pylon)
             
-            elif len(self.units(GATEWAY)) < len(self.units(NEXUS)) * 2:
+            elif len(self.units(GATEWAY)) < ((len(self.units(NEXUS)) * 3) - 2):
                 if self.can_afford(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
  
@@ -62,13 +62,20 @@ class MacroTossBot(sc2.BotAI):
         if self.units(NEXUS).amount < 4 and self.units(PROBE).amount > self.units(NEXUS).amount * 18 and self.can_afford(NEXUS) and not self.already_pending(NEXUS) and self.units(GATEWAY).amount > 0:
             await self.expand_now()
 
+    async def build_forge(self):
+        pylon = self.units(PYLON).ready
+        if pylon.exists:
+            if self.can_afford(FORGE):
+                await self.build(FORGE, near=pylon.closest_to(nexus))
+
     async def build_infantry_units(self):
         for gw in self.units(GATEWAY).ready.noqueue:
-            if self.can_afford(STALKER) and self.supply_left > 0 and self.units(CYBERNETICSCORE).exists:
+            if self.can_afford(STALKER) and self.supply_left > 0 and self.units(CYBERNETICSCORE).exists and self.units(STALKER).amount <= self.units(ZEALOT).amount * 2:
                 await self.do(gw.train(STALKER))
-            else:
-                if self.can_afford(ZEALOT) and self.supply_left > 0 and not self.units(CYBERNETICSCORE).ready:
-                    await self.do(gw.train(ZEALOT))
+            elif self.can_afford(ZEALOT) and self.supply_left > 0 and not self.units(CYBERNETICSCORE).ready:
+                await self.do(gw.train(ZEALOT))
+            elif self.can_afford(ZEALOT) and self.units(STALKER).amount >= self.units(ZEALOT).amount * 2:
+                await self.do(gw.train(ZEALOT))
 
     def find_target(self, state):
         if len(self.known_enemy_units) > 0:
@@ -79,16 +86,20 @@ class MacroTossBot(sc2.BotAI):
             return self.enemy_start_locations[0]
     
     async def attack(self):
-        if self.units(STALKER).amount > 15:
-            for s in self.units(STALKER).idle:
-                await self.do(s.attack(self.find_target(self.state)))
+        #{UNIT: [n to attack, n to defend]}
+        army_comp = {ZEALOT: [7, 2],
+                     STALKER: [15, 4]}
+        for UNIT in army_comp:
+            if self.units(UNIT).amount > army_comp[UNIT][0] and self.units(UNIT).amount > army_comp[UNIT][1]:
+                for s in self.units(UNIT).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
 
-        elif self.units(STALKER).amount > 3:
-            if len(self.known_enemy_units) > 0:
-                for s in self.units(STALKER).idle:
-                    await self.do(s.attack(random.choice(self.known_enemy_units)))
+            elif self.units(UNIT).amount > army_comp[UNIT][1]:
+                if len(self.known_enemy_units) > 0:
+                    for s in self.units(UNIT).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, MacroTossBot()),
-    Computer(Race.Terran, Difficulty.Easy)
-], realtime=False)
+    Computer(Race.Terran, Difficulty.Hard)
+], realtime=True)
